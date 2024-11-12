391db6a4601d0fa86871c6192d98707ead59d4ca
Psihys <bogdan.s.0474@gmail.com>
2024-11-12 10:37:49 +0100

    s

---
+++ Chat-app/chat-app/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Footer from './commponents/footer/footer';
+import Header from './commponents/header/header';
+import Main from './commponents/main/main';
+
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Main/>
+        <Footer/>
+    </div>
+     
+
+  );
+}
+
+export default App;
